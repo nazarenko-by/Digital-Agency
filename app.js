39634f02bcddb9bf0111c680c8acb0d56656626e
nazarenko-by <bodean56@ukr.net>
2021-01-16 01:46:56 +0200

    edited mobile menu

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ function currentSlide(n) {
 
 function showSlides(n) {
     let i;
-    console.log(n)
     if (n > slides.length) {
         slideIndex = 1;
     }
@@ -39,3 +38,32 @@ function showSlides(n) {
 function noLink(e) {
     e.preventDefault();
 };
+
+
+
+//mobile menu
+let mobileMenu = document.querySelector(".nav-mobile-menu");
+let mainMenu = document.querySelector(".menu-items");
+let firstLevelMenu = document.querySelectorAll(".first-level")
+
+function openCloseMobileMenu() {
+    mobileMenu.classList.toggle("active-menu");
+    if (mobileMenu.classList.contains("active-menu")) {
+        mainMenu.classList.add("active-menu");
+    } else {
+        mainMenu.classList.remove("active-menu")
+        firstLevelMenu.forEach((btn) => {
+            if (btn.lastElementChild.classList.contains("active-menu")) {
+                btn.lastElementChild.classList.remove("active-menu");
+            }
+        })
+    }
+}
+
+mobileMenu.addEventListener("click", openCloseMobileMenu)
+
+firstLevelMenu.forEach((btn) => {
+    btn.addEventListener("click", function (e) {
+        btn.lastElementChild.classList.toggle("active-menu");
+    })
+})
