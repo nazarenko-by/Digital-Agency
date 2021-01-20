a07d5fdae69e166fc113c1f6b1e1f3106f5ed2a6
nazarenko-by <bodean56@ukr.net>
2021-01-20 18:28:58 +0200

    app.js file has been split

--- src/app.js
+++ src/app.js
@@ -1,45 +1,7 @@
-// Slider
-let slides = document.querySelectorAll(".slider-info")
-let dots = document.getElementsByClassName("slider-dots_item");
-let slideIndex = 1;
-let count = 1; // slider counter
-currentSlide(1)
-setInterval(function () { // repeat
-    if (count >= slides.length) {
-        count = 0;
-    }
-    count++;
-    currentSlide(count);
-}, 5000)
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    let i;
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "flex";
-    dots[slideIndex - 1].className += " active";
-}
-
 //noLink
 function noLink(e) {
     e.preventDefault();
 };
-
-
 
 //mobile menu
 let mobileMenu = document.querySelector(".nav-mobile-menu");
